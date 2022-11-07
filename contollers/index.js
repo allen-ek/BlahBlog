7c79a4c5854b817caa6d0a3b6303eae177364020
A.Klein <allenkleined@gmail.com>
2022-11-06 18:49:03 -0800

    added basic route and page

---
+++ contollers/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
